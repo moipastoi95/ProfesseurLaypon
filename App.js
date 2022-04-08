647c686ff8d34df3b33c6045af020c6bf290c0ab
moipastoi95 <vincent.ky09@gmail.com>
2022-04-08 23:48:34 +0200

    block on main page

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import io from 'socket.io-client';
 import Home from "./screen/Home";
@@ -7,39 +7,50 @@ import Manage from "./screen/Manage";
 
 const MyStack = () => {
   const [socket, setSocket] = useState(null)
-  
+
   useEffect(() => {
-    const newSocket = io(`http://backlaypon-env.eba-q7y4uvtd.eu-west-3.elasticbeanstalk.com/`, {transports: ['websocket']}); // http://${window.location.hostname}:3000
+    const newSocket = io(`http://backlaypon-env.eba-q7y4uvtd.eu-west-3.elasticbeanstalk.com/`, { transports: ['websocket'] }); // http://${window.location.hostname}:3000
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
 
-  return (
-  <Router>
-    <Routes>
-      <Route path="/" element={socket ? (
-          <div>
-            <Home socket={socket}/>
-          </div>
-        ) : (
-          <div>Not connected</div>
-        )} />
-      <Route path="/admin" element={socket ? (
-        <div>
-          <Admin socket={socket}/>
-        </div>
-      ) : (
-        <div>Not connected</div>
-      )} />
-      <Route path="/manage" element={socket ? (
-        <div>
-          <Manage socket={socket}/>
-        </div>
-      ) : (
-        <div>Not connected</div>
-      )} />
-    </Routes>
-  </Router>
+  // return (
+  // <Router>
+  //   <Routes>
+  //     <Route path="/" element={socket ? (
+  //         <div>
+  //           <Home socket={socket}/>
+  //         </div>
+  //       ) : (
+  //         <div>Not connected</div>
+  //       )} />
+  //     <Route path="/admin" element={socket ? (
+  //       <div>
+  //         <Admin socket={socket}/>
+  //       </div>
+  //     ) : (
+  //       <div>Not connected</div>
+  //     )} />
+  //     <Route path="/manage" element={socket ? (
+  //       <div>
+  //         <Manage socket={socket}/>
+  //       </div>
+  //     ) : (
+  //       <div>Not connected</div>
+  //     )} />
+  //   </Routes>
+  // </Router>
+  // )
+
+  return (<div>
+    {socket ? (
+      <div>
+        <Home socket={socket} />
+      </div>
+    ) : (
+      <div>Not connected</div>
+    )}
+  </div>
   )
 };
 
